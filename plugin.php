48e5214b8039d71ffef79e07609a90bc5213eb88
Daniel Schulz-Jackson <daniel@10x.agency>
2015-06-18 18:39:06 -0500

    initial commit

---
+++ plugin.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Plugin Name: Example Plugin - LeadPages Widget
+ * Description: A brief example plugin demonstrating some of the bare-bones basics of what I imagine might be expected of me for the role of WP Plugin Developer at LeadPages.
+ * Author: Daniel Schulz-Jackson
+ * Author URI: http://10x.agency/about
+ */
