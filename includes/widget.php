51607aac660dd4408b935f02957920a3b97504ab
Daniel Schulz-Jackson <daniel@10x.agency>
2015-06-18 21:45:21 -0500

    url and zoom are now fully and cleanly set in widget

--- includes/widget.php
+++ includes/widget.php
@@ -9,6 +9,8 @@
 // Creating the widget 
 class wpb_widget extends WP_Widget {
 
+	const DEFAULT_LEADBOX_URL = 'https://my.leadpages.net/leadbox/14791da73f72a2%3A147c8da44b46dc/5682617542246400/';
+
 	function __construct() {
 		parent::__construct(
 			// Base ID of your widget
@@ -24,10 +26,7 @@ class wpb_widget extends WP_Widget {
 
 	// Creating widget front-end
 	// This is where the action happens
-	public function widget( $args, $instance ) {
-		$title = apply_filters( 'widget_title', $instance['title'] );
-		
-		// if (!$instance['zoom']) $instance['zoom'] = .67; // CHEATING!!!
+	public function widget( $args, $instance ) {		
 
 		// before and after widget arguments are defined by themes
 		echo $args['before_widget'];
@@ -58,7 +57,7 @@ class wpb_widget extends WP_Widget {
 		</style>
 		<div id="wrap">
 			<iframe 
-				src="https://my.leadpages.net/leadbox/14791da73f72a2%3A147c8da44b46dc/5682617542246400/"
+				src="<?php echo $instance['url']; ?>"
 				frameborder="0"
 				id="frame"
 			></iframe>
@@ -69,25 +68,43 @@ class wpb_widget extends WP_Widget {
 
 	// Widget Backend 
 	public function form( $instance ) {
-		if ( isset( $instance[ 'title' ] ) ) {
-			$title = $instance[ 'title' ];
+		if ( isset( $instance[ 'url' ] ) && $instance['url'] ) {
+			$url = $instance[ 'url' ];
 		}
 		else {
-			$title = __( 'New title', 'wpb_widget_domain' );
+			$url = self::DEFAULT_LEADBOX_URL;
 		}
 		if ( isset( $instance[ 'zoom' ] ) ) {
 			$zoom = $instance[ 'zoom' ];
 		}
 		else {
-			$zoom = __( '.9', 'wpb_widget_domain' );
+			$zoom = .9;
 		}
 		// Widget admin form
 		?>
 		<p>
-			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
-			<label for="<?php echo $this->get_field_id( 'zoom' ); ?>"><?php _e( 'Zoom:' ); ?></label>
-			<input type="number" min=".3" max="2" step="0.01" id="<?php echo $this->get_field_id( 'zoom' ); ?>" name="<?php echo $this->get_field_name( 'zoom' ); ?>" value="<?php echo $instance['zoom']; ?>" />
+			<label 
+				for="<?php echo $this->get_field_id( 'url' ); ?>"><?php _e( 'Leadbox URL:' ); ?>
+				</label>
+			<input 
+				id="<?php echo $this->get_field_id( 'url' ); ?>" 
+				name="<?php echo $this->get_field_name( 'url' ); ?>" 
+				type="text" 
+				value="<?php echo esc_attr( $url ); ?>" 
+			/>
+			<br>
+			<label 
+				for="<?php echo $this->get_field_id( 'zoom' ); ?>"><?php _e( 'Zoom:' ); ?>
+				</label>
+			<input 
+				id="<?php echo $this->get_field_id( 'zoom' ); ?>" 
+				name="<?php echo $this->get_field_name( 'zoom' ); ?>" 
+				type="number" 
+				min=".3" 
+				max="2" 
+				step="0.01" 
+				value="<?php echo esc_attr( $zoom ); ?>" 
+			/>
 		</p>
 		<?php 
 	}
@@ -95,7 +112,7 @@ class wpb_widget extends WP_Widget {
 	// Updating widget replacing old instances with new
 	public function update( $new_instance, $old_instance ) {
 		$instance = array();
-		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		$instance['url'] = ( ! empty( $new_instance['url'] ) ) ? strip_tags( $new_instance['url'] ) : '';
 		$instance['zoom'] = ( ! empty( $new_instance['zoom'] ) ) ? $new_instance['zoom'] : '';
 		return $instance;
 	}
